7bbd974d1eda2c271d7263947c734538e7940789
Sarnai1008 <121071604+Sarnai1008@users.noreply.github.com>
2023-04-10 09:46:24 +0800

    title

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './component/app';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
 root.render(
-  <h1>Hello world...</h1> 
+  <App />
 );
 
 // ReactDOM.render(
